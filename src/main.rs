3d66382387991e3a3b78815096d5909417a808e1
Stephen Duncan <jrduncans@users.noreply.github.com>
2019-10-20 11:08:51 -0700

    Print UTC and local time. Default to now.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use structopt::StructOpt;
 use chrono::prelude::*;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "timeturner", about = "Manipulate date-time strings")]
 struct Opt {
-    input: String
+    input: Option<String>,
 }
 
 fn main() {
     let opt = Opt::from_args();
-    if let Ok(epoch_millis) = opt.input.parse::<i64>() {
-        let date = Local.timestamp_millis(epoch_millis);
-        println!("{}", date.to_rfc3339());
-    }
+    let date_time = opt
+        .input
+        .map(|i| {
+            i.parse::<i64>()
+                .map(|e| Utc.timestamp_millis_opt(e).unwrap())
+                .unwrap()
+        })
+        .unwrap_or_else(|| Utc::now());
+    println!("{}", date_time.to_rfc3339());
+    println!("{}", date_time.with_timezone(&Local).to_rfc3339());
 }
