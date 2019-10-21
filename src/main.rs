72d5af39b7f8ca03bf1faee984ab414e349f4cd8
Stephen Duncan <jrduncans@users.noreply.github.com>
2019-10-20 21:24:36 -0700

    Parse RFC3339 or epoch millis. Handle errors better.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process;
+
 use chrono::prelude::*;
 use structopt::StructOpt;
 
@@ -7,16 +9,77 @@ struct Opt {
     input: Option<String>,
 }
 
+#[derive(PartialEq)]
+enum DateTimeFormat {
+    Missing,
+    EpochMillis,
+    Rfc3339,
+}
+
+struct ParsedInput {
+    input_format: DateTimeFormat,
+    value: DateTime<Utc>,
+}
+
+impl DateTimeFormat {
+    fn parse(&self, input: &str) -> Option<ParsedInput> {
+        match self {
+            DateTimeFormat::EpochMillis => parse_from_epoch_millis(input),
+            DateTimeFormat::Rfc3339 => parse_from_rfc3339(input),
+            DateTimeFormat::Missing => None,
+        }
+    }
+}
+
+fn parse_from_epoch_millis(input: &str) -> Option<ParsedInput> {
+    input
+        .parse::<i64>()
+        .ok()
+        .and_then(|e| Utc.timestamp_millis_opt(e).single())
+        .map(|d| ParsedInput {
+            input_format: DateTimeFormat::EpochMillis,
+            value: d,
+        })
+}
+
+fn parse_from_rfc3339(input: &str) -> Option<ParsedInput> {
+    DateTime::parse_from_rfc3339(input)
+        .ok()
+        .map(|d| ParsedInput {
+            input_format: DateTimeFormat::Rfc3339,
+            value: d.with_timezone(&Utc),
+        })
+}
+
+fn parse_input(input: Option<String>) -> Result<ParsedInput, &'static str> {
+    input
+        .map(|i| {
+            DateTimeFormat::EpochMillis
+                .parse(&i)
+                .or(DateTimeFormat::Rfc3339.parse(&i))
+                .ok_or("Input format not recognized")
+        })
+        .unwrap_or_else(|| {
+            Ok(ParsedInput {
+                input_format: DateTimeFormat::Missing,
+                value: Utc::now(),
+            })
+        })
+}
+
 fn main() {
     let opt = Opt::from_args();
-    let date_time = opt
-        .input
-        .map(|i| {
-            i.parse::<i64>()
-                .map(|e| Utc.timestamp_millis_opt(e).unwrap())
-                .unwrap()
-        })
-        .unwrap_or_else(|| Utc::now());
-    println!("{}", date_time.to_rfc3339());
-    println!("{}", date_time.with_timezone(&Local).to_rfc3339());
+    let parsed_input = parse_input(opt.input).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    if parsed_input.input_format != DateTimeFormat::Rfc3339 {
+        println!("{}", parsed_input.value.to_rfc3339());
+        println!("{}", parsed_input.value.with_timezone(&Local).to_rfc3339());
+    }
+
+    if parsed_input.input_format != DateTimeFormat::EpochMillis {
+        println!("{}", parsed_input.value.timestamp_millis());
+    }
 }
